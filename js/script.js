8f36928c2b53736a3951986013ca2b06951b74bc
Andreasgura <andre-56@hotmail.it>
2024-03-18 15:31:12 +0100

    display you lose on console.log

--- js/script.js
+++ js/script.js
@@ -16,24 +16,35 @@ creo un div.grid   .d-flex-justify-coontent-center.align-items-center
 dentro il div inserisco N volte una fz che mi genera un quadrato. ogni quadrato ha all'interno, sempre al centro (quindi aggiungo classi), un numero (o in generale qualcosa di scritto)
 
 */
-/*
 
-let elBtnPlay = document.getElementById('playbtn')
-let elBoxContainer = document.getElementById('box-container')
-elBtnPlay.addEventListener('click', function(){
+let elBtnPlay = document.getElementById('playbtn');
+let elBoxContainer = document.getElementById('box-container');
+elBtnPlay.addEventListener('click', function () {
     elBoxContainer.innerHTML = '';
+    let arrayBombs = [];
+    console.log('vuoto' + arrayBombs)
+    for (let index = 0; index < 16; index++) {
+    const newBomb = generateUniqueRandomnumbers(1, 100, arrayBombs);
+    arrayBombs.push(newBomb);
+    }
     let boxes;
     for (let index = 0; index < 100; index++) {
-        boxes = generateSquares(index + 1); 
+        boxes = generateSquares(index + 1);
         elBoxContainer.appendChild(boxes);
-        boxes.addEventListener ('click', function () {
-            this.classList.add('clicked')
-            console.log(index + 1)
-            elBoxContainer.appendChild(boxes);   
+        boxes.addEventListener('click', function () {
+            if(arrayBombs.includes(index + 1)) {
+                this.classList.add('end-game')
+                console.log('hai perso')
+            } else {
+                this.classList.add('clicked')
+                console.log(arrayBombs)
+            }
+            
+            elBoxContainer.appendChild(boxes);
         });
-        
+
     }
-    
+
 
 });
 
@@ -46,31 +57,34 @@ function generateSquares(content) {
     newElement.innerHTML = `<span>${content}<span>`;
     return newElement;
 }
+
+/*
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+se il numero (presente nello span) è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle
+
 */
-// let blacklist = [1,3,5,7];
-// let randomNmb = Math.floor(Math.random() * (7 - 1) ) + 1;
-// let flag = false;
-// if (blacklist.includes(randomNmb)){
-//     console.log('è incluso' + randomNmb)
-// } else {
-//     flag = true
-//     console.log(flag + ' non è incluso ' + randomNmb)
-// };
 
-let mylist = [1,3]
-console.log(generateUniqueRandomnumbers (1, 6, mylist))
 
-function generateUniqueRandomnumbers (min, max, blacklist) {
+
+
+
+function generateUniqueRandomnumbers(min, max, blacklist) {
 
     let randomNotincluded;
     let flag = false;
 
     while (flag === false) {
-        randomNotincluded = Math.floor(Math.random() * (max - min + 1) ) + min
+        randomNotincluded = Math.floor(Math.random() * (max - min + 1)) + min
         console.log('nel ciclo' + randomNotincluded)
-        if (blacklist.includes(randomNotincluded) === false){
+        if (blacklist.includes(randomNotincluded) === false) {
             flag = true;
         }
     }
     return randomNotincluded;
 }
+
