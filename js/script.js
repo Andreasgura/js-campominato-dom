4891d52df88aa42a6e8a961fa74e0e5a485ad29a
Andreasgura <andre-56@hotmail.it>
2024-03-18 18:21:52 +0100

    end game

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-/*
-Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
-Bonus
-Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-
-
-creo un div.grid   .d-flex-justify-coontent-center.align-items-center
-dentro il div inserisco N volte una fz che mi genera un quadrato. ogni quadrato ha all'interno, sempre al centro (quindi aggiungo classi), un numero (o in generale qualcosa di scritto)
-
-*/
 
 let elBtnPlay = document.getElementById('playbtn');
 let elBoxContainer = document.getElementById('box-container');
@@ -30,23 +12,25 @@ function play() {
     arrayBombs.push(newBomb);
     }
     let boxes;
+    let bomb = false;
+    let point = 0
     for (let index = 0; index < 100; index++) {
         boxes = generateSquares(index + 1);
         elBoxContainer.appendChild(boxes);
-        let bomb = false;
         boxes.addEventListener('click', function () {
+            if (bomb === true) {
+                return;
+            }
             if(arrayBombs.includes(index + 1)) {
                 this.classList.add('end-game')
-                console.log('hai perso')
+                console.log('hai perso. Il tuo punteggio è ' + point)
                 bomb = true
             } else {
                 this.classList.add('clicked')
                 console.log(arrayBombs)
+                point = point + 1
             }
             elBoxContainer.appendChild(boxes);
-            if (bomb === true) {
-                return
-            }
         }, {once: true} );
 
     }
@@ -63,21 +47,6 @@ function generateSquares(content) {
     newElement.innerHTML = `<span>${content}<span>`;
     return newElement;
 }
-
-/*
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-
-
-se il numero (presente nello span) è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle
-
-*/
-
-
-
-
 
 function generateUniqueRandomnumbers(min, max, blacklist) {
 
